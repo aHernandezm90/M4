b156a47e8ec535357200b8e2f8c3fc2faa1af079
carlescarmonacalpe <carlescarmonacalpe@gmail.com>
2016-01-05 14:23:09 +0100

    Graficos Task2

--- week2/main.m
+++ week2/main.m
@@ -6,8 +6,8 @@
 param.gaussianColor = false;
 %Value between [-2.5,2.5]
 param.alpha = 0.1;
-param.maxAlpha = 2.5;
-param.minAlpha = -2.5;
+param.maxAlpha = 5.0;
+param.minAlpha = 0;
 
 %------------------
 %Gaussian Modelling
@@ -15,14 +15,10 @@ param.minAlpha = -2.5;
 
 %-------Task 1-----------
 
-
-
 %Get the Datasets
 datasets = dir('Dataset/');
 datasets = datasets(find(vertcat(datasets.isdir)));
 datasets = datasets(3:end);
-
-
 
 for i=1:length(datasets)
     
@@ -40,7 +36,7 @@ for i=1:length(datasets)
     
     %Testing
     %-------
-%     [output{i}] = GaussianTesting(inputFolder,dirList,mean_dataset{i},sd_dataset{i},numberTraining,param.gaussianColor,param.alpha,true);
+    %[output{i}] = GaussianTesting(inputFolder,dirList,mean_dataset{i},sd_dataset{i},numberTraining,param.gaussianColor,param.alpha,true);
 
 end
 
@@ -82,4 +78,42 @@ for i=1:length(datasets)
         t=t+1;
     end
 end
+
+%Ploting Data
+for i=1:length(datasets)
+    len = length(TP(i,:));
+    figure,
+    plot((1:len),TP(i,:),(1:len), FP(i,:),(1:len), FN(i,:),(1:len), TN(i,:))
+%     set(gca,'XTick',1:10:len )
+     set(gca,'XTickLabel',param.minAlpha:0.1*10:param.maxAlpha )
+    legend('TP','FP','FN','TN')
+    title(strcat('Evaluation - Non-recursive Gaussian modeling - "',datasets(i).name,'" Dataset'))
+    xlabel('\alpha Threshold')
+    ylabel('Nº Pixels')
+end
+
+for i=1:length(datasets)
+    figure,
+    plot(1:length(F1(i,:)),F1(i,:))
+    %Max F1
+    [y x] = max(F1(i,:))
+    hold on
+    plot(x,y,'o','MarkerSize',10)
+    strmax = ['\alpha = ',num2str(x*0.1), ' | F1 Score = ',num2str(F1(i,x))];
+    text(x,y,strmax,'HorizontalAlignment','left');
+    %Min F1
+    [y x] = min(F1(i,:))
+    hold on
+    plot(x,y,'o','MarkerSize',10)
+    strmax = ['\alpha = ',num2str(x*0.1), ' | F1 Score = ',num2str(F1(i,x))];
+    text(x,y,strmax,'HorizontalAlignment','left');
+    %Plot parameters
+%     set(gca,'XTick',1:10:len )
+    set(gca,'XTickLabel',param.minAlpha:0.1*10:param.maxAlpha )
+    title(strcat('F1 Score - Non-recursive Gaussian modeling - "',datasets(i).name,'" Dataset'))
+    xlabel('\alpha Threshold')
+    ylabel('F1 Score')
+end
+
+
   
