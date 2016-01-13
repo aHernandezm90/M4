d8c210d9022f0494ff3e8b769a346e948a525522
Álvaro Hernández Martín <alvaro.hernandezm@e-campus.uab.cat>
2016-01-13 23:06:47 +0100

    task1&2

--- week3/task1.m
+++ week3/task1.m
@@ -3,37 +3,54 @@
 %anteriormente que estaran en las rutas datasetTrafficPath,
 %datasetHighwayPath y datasetFallPath.Se almacenaran en la carpeta results
 %conn4 y conn8 dependiendo de la conectividad.
-datasetTrafficPath = '../week3/input/Traffic/';
-datasetHighwayPath = '../week3/input/Highway/';
-datasetFallPath = '../week3/input/Fall/';
-resultsTrafficPath = '../week3/results/imfill/conn8/Traffic/';
-resultsHighwayPath = '../week3/results/imfill/conn8/Highway/';
-resultsFallPath = '../week3/results/imfill/conn8/Fall/';
+datasetTrafficPath = 'input/Traffic/';
+datasetHighwayPath = 'input/Highway/';
+datasetFallPath = 'input/Fall/';
+resultsTrafficPath = 'results/imfill/conn8/Traffic/';
+resultsHighwayPath = 'results/imfill/conn8/Highway/';
+resultsFallPath = 'results/imfill/conn8/Fall/';
 connectivity = 8; %Cambiar conectividad, 4 u 8
 locations = [3 3];
 datasetPath = {datasetTrafficPath,datasetHighwayPath,datasetFallPath};
 resultsPath = {resultsTrafficPath,resultsHighwayPath,resultsFallPath};
-%Build dir for placing results
-for i = 1:3
-    mkdir(datasetPath{i});
-end
-for i = 1:3
-    mkdir(resultsPath{i});
-end
+datasetsNames = {'Traffic','Highway','Fall'};
+datasets = {'../Dataset/Traffic/input/','../Dataset/Highway/input/','../Dataset/Fall/input/'};
+% %Build dir for placing results
+% for i = 1:3
+%     mkdir(datasetPath{i});
+% end
+% for i = 1:3
+%     mkdir(resultsPath{i});
+% end
+% 
+% for i = 1:3
+%     %Apply hole filling
+%     %Obtain list of files
+%     listFiles = dir(datasetPath{i});
+%     listFiles = listFiles(3:end);
+%     numFiles = length(listFiles);
+%     %Compute every image
+%     for j = 1:numFiles
+%         fileName = listFiles(j).name;
+%         originalImage = logical(imread(strcat(datasetPath{i},fileName)));
+%         filledImage = imfill(originalImage,connectivity,'holes');
+%         imwrite(filledImage,strcat(resultsPath{i},fileName));
+%     end
+% end
 
-for i = 1:3
-    %Apply hole filling
-    %Obtain list of files
-    listFiles = dir(datasetPath{i});
-    listFiles = listFiles(3:end);
-    numFiles = length(listFiles);
-    %Compute every image
-    for j = 1:numFiles
-        fileName = listFiles(j).name;
-        originalImage = logical(imread(strcat(datasetPath{i},fileName)));
-        filledImage = imfill(originalImage,connectivity,'holes');
-        imwrite(filledImage,strcat(resultsPath{i},fileName));
+
+for i=1:length(resultsPath)
+    imageNames = dir(fullfile(datasets{i},'*.jpg'));
+    nTraining = floor(size(imageNames,1)/2);
+    groundtruth = loadGroundtruth(datasetsNames{i});
+    groundtruth = groundtruth(:,:,nTraining+1:end);
+    outputNames = dir(fullfile(resultsPath{i},'*.jpg'));
+    for j = 1:nTraining+1
+        output(:,:,j) = logical(imread(strcat(resultsPath{i},outputNames(j).name)));
     end
+    disp(strcat('-----Dataset:',datasetsNames{i},'-----'));
+    disp(strcat('F1:',num2str(F1),'Precision:',num2str(Precision),'Recall:',num2str(Recall)));
+    output = [];
 end
 
 
--- week3/task2.m
+++ week3/task2.m
@@ -1,29 +1,49 @@
-%Se toma como input la salida de task 1 (conn4 o conn 8, el que haya dado mejor F1)
+%Se toma como input la salida de task 1 (conn4 que es el que ha dado mejores resultados)
 %Task2
-datasetTrafficPath = '../week3/input/Traffic/';
-datasetHighwayPath = '../week3/input/Highway/';
-datasetFallPath = '../week3/input/Fall/';
-resultsTrafficPath = '../week3/results/bwareaopen/Traffic/';
-resultsHighwayPath = '../week3/results/bwareaopen/Highway/';
-resultsFallPath = '../week3/results/bwareaopen/Fall/';
+datasetTrafficPath = 'input/Traffic/';
+datasetHighwayPath = 'input/Highway/';
+datasetFallPath = 'input/Fall/';
 datasetPath = {datasetTrafficPath,datasetHighwayPath,datasetFallPath};
-resultsPath = {resultsTrafficPath,resultsHighwayPath,resultsFallPath};
-%Build dir for placing results
-for i = 1:3
-    mkdir(resultsPath{i});
-end
-
-for i = 1:3
-    %Apply hole filling
-    %Obtain list of files
-    listFiles = dir(datasetPath{i});
-    listFiles = listFiles(3:end);
-    numFiles = length(listFiles);
-    %Compute every image
-    for j = 1:numFiles
-        fileName = listFiles(j).name;
-        originalImage = logical(imread(strcat(datasetPath{i},fileName)));
-        filteredImage = bwareaopen(originalImage,20);
-        imwrite(filteredImage,strcat(resultsPath{i},fileName));
+datasetsNames = {'Traffic','Highway','Fall'};
+datasets = {'../Dataset/Traffic/input/','../Dataset/Highway/input/','../Dataset/Fall/input/'};
+tam = [1 3 5 7 10 12 15 20 30 50];
+%Build dir and compute bwareaopen 
+for i = 1:length(tam)
+    pathGen = strcat('results/bwareaopen/tam',num2str(tam(i)),'/');
+    mkdir(pathGen);
+    for j = 1:3
+        mkdir(strcat(pathGen,datasetsNames{j}));
+        listFiles = dir(fullfile(datasetPath{j},'*.jpg'));
+        numFiles = length(listFiles);
+        for k = 1:numFiles
+            fileName = listFiles(k).name;
+            originalImage = logical(imread(strcat(datasetPath{j},fileName)));
+            %filteredImage = bwareaopen(originalImage,tam(i));
+            filteredImage = originalImage;
+            imwrite(filteredImage,strcat(pathGen,datasetsNames{j},'/',fileName));
+        end
     end
 end
+
+%Evaluate results
+for i = 1:length(tam)
+    disp(strcat('::::::::::::Tamaño ',num2str(tam(i)),':::::::::::::::'));
+    pathGen = strcat('results/bwareaopen/tam',num2str(tam(i)),'/');
+    for j = 1:3
+        imageNames = dir(fullfile(datasets{j},'*.jpg'));
+        nTraining = floor(size(imageNames,1)/2);
+        groundtruth = loadGroundtruth(datasetsNames{j});
+        groundtruth = groundtruth(:,:,nTraining+1:end);
+        outputNames = dir(fullfile(strcat(pathGen,datasetsNames{j}),'*.jpg'));
+        for k = 1:nTraining+1
+            output(:,:,k) = logical(imread(strcat(pathGen,datasetsNames{j},'/',outputNames(k).name)));
+        end
+        [TP,FP,FN,TN,F1,Recall,Precision] = datasetEvaluation(output,groundtruth);
+        disp(strcat('-----Dataset:',datasetsNames{j},'-----'));
+        disp(strcat('F1:',num2str(F1),'Precision:',num2str(Precision),'Recall:',num2str(Recall)));
+        output = [];
+    end
+end
+
+
+
